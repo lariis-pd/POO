e53b18accc4809679d3cce1afd171d808f9c4df0
lariis-pd <20233005626@estudantes.ifpr.edu.br>
2025-03-18 11:01:15 -0300

    aoba

--- aula/TestaFuncionario.java
+++ aula/TestaFuncionario.java
@@ -1,10 +1,15 @@
 package aula;
-public class TestaGerente {
+public class TestaFuncionario {
     
     public static void main(String[] args) {
         Gerente G1 = new Gerente();
         
         G1.setNome("Larissa");
+        G1.setCpf("02365423599");
+        G1.setSalario(6000);
+        G1.setNumFuncionarios(8);
         G1.setSenha(0702);
+
+        
     }
 }
